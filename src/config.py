7333f8fd9cf21e0d69593951dcb25ebeeb25bcec
tin3ga <118301331+tin3ga@users.noreply.github.com>
2025-01-03 16:12:53 +0300

    Enhance configuration and logging with detailed docstrings for ETL pipeline

--- src/config.py
+++ src/config.py
@@ -5,5 +5,16 @@ load_dotenv()
 
 
 class Config:
+    """
+    Configuration class for the ETL pipeline.
+
+    Attributes:
+        URL (str): The URL to fetch user data from.
+        JSON_FILE (str): The filename to save the raw user data in JSON format.
+        CSV_FILE (str): The filename to save the transformed user data in CSV format.
+        DATABASE_URL (str): The database connection URL, fetched from environment variables.
+    """
     URL = 'https://dummyjson.com/users'
     JSON_FILE = 'user_data.json'
+    CSV_FILE = 'transformed_user_data.csv'
+    DATABASE_URL = os.getenv('DATABASE_URL')
--- src/utils.py
+++ src/utils.py
@@ -7,5 +7,14 @@ logging.basicConfig(
 
 
 def setup_logger(name: str) -> logging.Logger:
+    """
+    Sets up and returns a logger with the specified name.
+
+    Args:
+        name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
     lgr = logging.getLogger(name)
     return lgr
